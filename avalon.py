9e3d6d37ed8b9d6e4d0267d5ac971765130476f4
Cloumeau <C2loumeau@gmail.com>
2023-05-11 15:19:29 -0400

    Add files via upload

--- avalon.py
+++ avalon.py
@@ -18,3 +18,61 @@ else:
     print("Player characters:")
     for i in range(num_players):
         print(f"Player {i+1}: {player_characters[i]}")
+
+# Set up player order and quest sizes based on number of players
+player_order = [i+1 for i in range(num_players)]
+random.shuffle(player_order)
+quest_sizes = {5: [2, 3, 2, 3, 3],
+               6: [2, 3, 4, 3, 4],
+               7: [2, 3, 3, 4, 4],
+               8: [3, 4, 4, 5, 5],
+               9: [3, 4, 4, 5, 5],
+               10: [3, 4, 4, 5, 5]}
+
+# Initialize quest scores as an empty list
+quest_scores = []
+quest_scores = [None] * 5
+# Print out player order
+print("Player order: ", player_order)
+
+quest_num = 1
+num_players_on_quest = quest_sizes[num_players][quest_num-1]
+print(f"\nQuest {quest_num}: Select {num_players_on_quest} players for the quest.")
+
+players_on_quest = []
+for i in range(num_players_on_quest):
+    valid_choice = False
+    while not valid_choice:
+        player_choice = int(input(f"Choose your {i+1} player: "))
+        if player_choice not in players_on_quest and player_choice in player_order:
+            players_on_quest.append(player_choice)
+            valid_choice = True
+        else:
+            print("Invalid choice. Please choose a different player.")
+
+print("Players on the quest: ", players_on_quest)
+
+votes = []
+for i in range(num_players_on_quest):
+    valid_choice = False
+    while not valid_choice:
+        vote_choice = input(f"Player {players_on_quest[i]}, vote succeed or fail: ")
+        if vote_choice.lower() == "succeed" or vote_choice.lower() == "fail":
+            votes.append(vote_choice.lower())
+            valid_choice = True
+        else:
+            print("Invalid choice. Please choose succeed or fail.")
+
+num_fails = votes.count("fail")
+if num_fails == 0:
+    print("Quest succeeded!")
+    quest_result = "Succeed"
+else:
+    print("Quest failed!")
+    quest_result = "Fail"
+    
+quest_scores[quest_num-1] = quest_result
+
+print("Current quest scores:")
+for i, score in enumerate(quest_scores):
+    print(f"Quest {i+1}: {score}")
